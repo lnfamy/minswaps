932e827254b888b9eca64fdc4c8c6a3ec47ac1ec
river <rivermalachi@gmail.com>
2023-12-26 14:19:26 +0200

    add

---
+++ Minimum number of substrings with unique characters.py
@@ -0,0 +1,16 @@
+def solution(s):
+    last_seen = [-1] * 26
+    start = 0
+    ans = 1
+
+    for i in range(len(s)):
+        # if the last time we saw this character was after the start of this current substring, we need to start a
+        # new substring. that means we have one more substring, therefore we increment the answer by 1 and set the
+        # start index of the current substring to the current index i.
+        if last_seen[ord(s[i]) - ord('a')] >= start:
+            ans += 1
+            start = i
+        # set the last seen position of this character to its index in the string
+        last_seen[ord(s[i]) - ord('a')] = i
+
+    return ans
